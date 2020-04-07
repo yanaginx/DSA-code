659f70ad0b403a0fe7d1678e443a4e40b0f120fd
Duong <duongchan159@gmail.com>
2020-04-07 15:30:41 +0700

    Added 'Checking balanced parenthesis using stack'

---
+++ prob-w-stack.cpp
@@ -0,0 +1,63 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  prob-w-stack.cpp
+ *
+ *    Description:  Using stack to checking the balances of parenthesis
+ *
+ *        Version:  1.0
+ *        Created:  04/07/2020 03:06:44 PM
+ *       Revision:  none
+ *       Compiler:  g++
+ *
+ *         Author:  Duong V.Chan
+ *        Company:  HCMUT
+ *
+ * =====================================================================================
+ */
+
+#include<iostream>
+#include<string>
+#include<stack>
+
+using namespace std;
+
+bool CheckParanthesis ( char opening, char closing )
+{
+   if ( opening == '(' && closing == ')' )
+      return true;
+   else if ( opening == '{' && closing == '}' )
+      return true;
+   else if ( opening == '[' && closing == ']' )
+      return true;
+   else 
+      return false;
+}
+
+bool CheckBalanced ( string exp )
+{
+   stack<char> S;
+   int n = exp.length(); 
+   for ( int i = 0; i < n; i++ )
+   {
+      if ( exp[i] == '{' || exp[i] == '(' || exp[i] == '[' )
+         S.push( exp[i] );
+      else if ( exp[i] == '}' || exp[i] == ')' || exp[i] == ']' )
+      {
+         if ( S.empty()  || !CheckParanthesis(S.top(), exp[i]) ) 
+            return false;
+         else S.pop();
+      }
+   }      
+   if ( S.empty() )
+      return true;
+   else
+      return false;
+}
+
+int main()
+{
+   string a = "[()(){}{}{}]";
+   cout << CheckBalanced(a);
+   return 0;
+}
